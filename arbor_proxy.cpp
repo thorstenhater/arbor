6ea3579473cf1611c4c12ea3d2f656d010c18940
Alexander Peyser <a.peyser@fz-juelich.de>
2018-09-14 10:59:59 +0200

    nest -> arbor proxy

--- arbor_proxy.cpp
+++ arbor_proxy.cpp
@@ -11,7 +11,7 @@ struct ArborSpike {
 };
 typedef std::vector< ArborSpike > ArborSpikes;
 
-void work(int global_rank, int global_size,
+void work(int global_rank, int global_size, int local_rank,
 	  float min_delay, float run_time)
 {
   const int sbuf_length = 0;
@@ -20,8 +20,8 @@ void work(int global_rank, int global_size,
   const std::vector<int> sbuf(1);
 
   for (; run_time > 0; run_time -= min_delay) {
-    if (global_rank == 0) {
-      std::cout << "Time left: " << run_time << std::endl;
+    if (local_rank == 0) {
+      std::cerr << "Time left: " << run_time << std::endl;
     }
     
     MPI_Allgather(&sbuf_length, 1, MPI_INT,
@@ -46,12 +46,12 @@ void work(int global_rank, int global_size,
 		   &rbuf[0], &rbuf_lengths[0], &rbuf_offset[0], MPI_CHAR,
 		   MPI_COMM_WORLD);
 
-    if (global_rank == 0) {
+    if (local_rank == 0) {
       for (auto spike: rbuf) {
 	auto gid = spike.gid;
 	auto time = spike.time;
 	
-	std::cout << "Gid: " << gid << ", Time: " << time << std::endl;
+	std::cerr << "Gid: " << gid << ", Time: " << time << std::endl;
       }
     }
   }
@@ -68,7 +68,7 @@ int main(int argc, char **argv)
   MPI_Init(&argc, &argv); 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  
+
   /* Build intra-communicator for local sub-group */
   MPI_Comm   intracomm;  /* intra-communicator of local sub-group */ 
   MPI_Comm_split(MPI_COMM_WORLD, 0, rank, &intracomm);
@@ -76,9 +76,13 @@ int main(int argc, char **argv)
   int lrank, lsize;
   MPI_Comm_rank(intracomm, &lrank);
   MPI_Comm_size(intracomm, &lsize);
+  if (lrank == 0) {
+    std::cerr << "Starting arbor_proxy: size=" << size << ", min_delay=" << min_delay << ", run_time=" << run_time << std::endl;
+    std::cerr << "Local size: " << lsize << std::endl;
+  }
   
   // work
-  work(rank, size, min_delay, run_time);
+  work(rank, size, lrank, min_delay, run_time);
 
   // cleanup
   MPI_Comm_free(&intracomm);  
