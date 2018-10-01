03373c5a21984537f17cb5fd2d2367bb19809d50
Alexander Peyser <a.peyser@fz-juelich.de>
2018-10-01 11:13:17 +0200

    simplify nest_sender with nestio

--- example/inter/nest_sender.py
+++ example/inter/nest_sender.py
@@ -1,14 +1,9 @@
 #! /usr/bin/python
 
 from mpi4py import MPI
-
 comm = MPI.COMM_WORLD.Split(1)
-#print(comm)
 
-#import os
-#os.environ['DELAY_PYNEST_INIT'] = "1"
 import nest
-
 nest.set_communicator(comm)
 
 pg = nest.Create('poisson_generator', params={'rate': 10.0})
@@ -21,10 +16,10 @@ nest.Connect(parrots, sd)
 #print(nest.GetKernelStatus())
 #nest.SetKernelStatus({'recording_backend': 'arbor'})
 #nest.SetKernelStatus({'recording_backends': {'screen': {}}})
-status = nest.GetKernelStatus()
-print('min_delay: ', status['min_delay'], ", max_delay: ", status['max_delay'])
-nest.SetKernelStatus({'min_delay': status['min_delay']/2,
-                      'max_delay': status['max_delay']})
-status = nest.GetKernelStatus()
-print('min_delay: ', status['min_delay'], ", max_delay: ", status['max_delay'])
-nest.Simulate(1000.0)
+# status = nest.GetKernelStatus()
+# print('min_delay: ', status['min_delay'], ", max_delay: ", status['max_delay'])
+# nest.SetKernelStatus({'min_delay': status['min_delay']/2,
+#                       'max_delay': status['max_delay']})
+# status = nest.GetKernelStatus()
+# print('min_delay: ', status['min_delay'], ", max_delay: ", status['max_delay'])
+nest.Simulate(100.0)
