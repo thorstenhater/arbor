3bf726b6d7f03923d81b982a3367c43be37a0171
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2026-05-29 15:05:03 +0200

    Move stream arbenv::cuda => arb::cuda

--- arbor/include/arbor/gpu/cuda_api.hpp
+++ arbor/include/arbor/gpu/cuda_api.hpp
@@ -14,6 +14,8 @@ namespace gpu {
 
 using DeviceProp = cudaDeviceProp;
 
+using gpu_stream = cudaStream_t;
+
 struct ARB_SYMBOL_VISIBLE api_error_type {
     cudaError_t value;
     api_error_type(cudaError_t e): value(e) {}
